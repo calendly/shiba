51e01d299cce7ab165bddefb09dd4742e0a00198
Bill Siggelkow <bill@calendly.com>
2019-06-04 08:15:38 -0400

    dereference procs

--- lib/shiba/query_watcher.rb
+++ lib/shiba/query_watcher.rb
@@ -34,6 +34,7 @@ module Shiba
     end
 
     def interpolate(sql, binds)
+      binds = binds.call if Proc === binds # dereference procs
       binds.each_with_index do |val, i|
         sql = sql.sub("$#{i +1}", ActiveRecord::Base.connection.quote(val))
       end
